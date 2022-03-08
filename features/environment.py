00d8aba0ef97ec3161a2e537e7ff216f9f9533a9
alexandrkardash <alexandr.kardash@coinspaid.com>
2022-03-08 12:01:25 +0300

    update tag

--- features/environment.py
+++ features/environment.py
@@ -64,9 +64,7 @@ def before_all(context):
     parser.read('behave.ini')
     context.config = parser
     context.values = {}
-    print(f'remove email')
     gmail.delete_all_emails()
-    print('removed')
 
 
 def before_feature(context, feature):
